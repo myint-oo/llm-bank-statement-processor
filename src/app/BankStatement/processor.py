812ef481cbd9a552b94654f83d1e6d650e4cbf89
Myint Oo <themyintoo@gmail.com>
2025-07-14 15:43:09 +0700

    increased max_new_tokens for full output

--- src/app/BankStatement/processor.py
+++ src/app/BankStatement/processor.py
@@ -69,7 +69,7 @@ class BankStatementProcessor:
             outputs = self.model.generate(
                 input_ids=input_ids,
                 attention_mask=attention_mask,
-                max_new_tokens=1024,
+                max_new_tokens=2048,
                 eos_token_id=self.tokenizer.eos_token_id,
                 pad_token_id=self.tokenizer.pad_token_id,
             )
